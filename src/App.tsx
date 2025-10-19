47b9d2c2beb051e91e86b2e3ed3c76e06b1d6a7c
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-19 12:30:08 +0500

    savw

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import CalculateComp from './components/CalculateComp'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* value pass in props 
+      value pass in state */}
+    {/* < CalculateComp  length={5} width={7}/> */}
+    <CalculateComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CalculateComp.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+
+interface CalculateCompProps {
+  length?: number
+  width?: number
+}
+const CalculateComp = ({ length=1, width=1}: CalculateCompProps) => {
+const [Length, setLength]= useState(length)
+const [Width, setWidth]= useState(width)
+const [result,setResult]= useState(1)
+
+
+const findarea = (length:number, width:number): void => {
+  const result = setResult(length*width)
+  return result
+}
+ 
+const findPerimeter = ( length:number, width:number):void => {
+  const result = setResult (2 * (length + width));
+  return result
+}
+
+  return (
+    <>
+    <div className="flex flex-col gap-4 w-70 ">
+      <input onChange={(e)=> setLength(Number(e.target.value))} type="text" value={Length} placeholder="Enter your length" />
+      <input onChange={(e)=> setWidth(Number(e.target.value))} type="text" value={Width} placeholder="Enter Your Width" />
+             <button className="bg-teal-600">Calculate Area</button>
+            <button className="bg-teal-600 ">Calculate Parameter</button>
+</div>
+      
+      
+
+    
+    </>
+  )
+}
+
+export default CalculateComp
